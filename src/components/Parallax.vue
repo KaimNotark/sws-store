db13556118036dc57db07130d9d008541370b1ff
KaimNotark <kaimnotark@yandex.ru>
2020-02-07 13:42:05 +0800

    done parallax

--- src/components/Parallax.vue
+++ src/components/Parallax.vue
@@ -1,25 +1,59 @@
 <template>
   <div class="parallax-container">
-    <!-- <img alt="StarWars Ship logo" src="/images/ship-logo.png" /> -->
     <section class="main">
       <div class="wrap wrap--1">
-        <div class="container container--1"></div>
+        <div
+          v-bind:style="{ 
+          backgroundPosition: styleObject.bX +' '+ styleObject.bY,
+          transform: 'rotate(' + styleObject.rX + ') rotateY('+ styleObject.rY + ')'
+           }"
+          class="container container--1"
+          @mousemove="handleMouseMove"
+          @mouseleave="handleMouseLeave"
+        ></div>
       </div>
     </section>
   </div>
 </template>
     
 <script>
-// import "./parallax.js";
-
 export default {
   name: "Parallax",
 
+  data() {
+    return {
+      styleObject: {
+        rX: "0deg",
+        rY: "0deg",
+        bX: "50%",
+        bY: "80%"
+      }
+    };
+  },
 
-  // parallax
-  // methods: {
+  methods: {
+    handleMouseMove(event) {
+      const { offsetX, offsetY } = event;
+      let X;
+      let Y;
 
-  // }
+      X = -(offsetX - 150) / 3 / 3;
+      Y = (offsetY - 180) / 3 / 3;
+
+      this.styleObject.rY = X.toFixed(2) + "deg";
+      this.styleObject.rX = Y.toFixed(2) + "deg";
+
+      this.styleObject.bY = 80 - (X / 4).toFixed(2) + "%";
+      this.styleObject.bX = 80 - (Y / 4).toFixed(2) + "%";
+    },
+
+    handleMouseLeave() {
+      this.styleObject.rY = "0deg";
+      this.styleObject.rX = "0deg";
+      this.styleObject.bY = "80%";
+      this.styleObject.bX = "50%";
+    }
+  }
 };
 </script>
 
@@ -47,23 +81,17 @@ export default {
 }
 
 .container {
-  --rX: 0;
-  --rY: 0;
-  --bX: 50%;
-  --bY: 80%;
   width: 30rem;
   height: 36rem;
-  border: 1px solid red;
+  border: 2px solid red;
   border-radius: 1.6rem;
   padding: 4rem;
   display: flex;
   align-items: flex-end;
   position: relative;
-  transform: rotate(calc(var(--rX) * ldeg)) rotateY(calc(var(--rv) * ldeg));
-  background: linear-gradient(hsla(0, 0%, 100%, 0.1), hsla(0, 0%, 100%, 0.1)),
+  background: linear-gradient(hsla(0, 0%, 100%, 0.1), hsla(0, 0%, 100%, 0.3)),
     url("/images/ship-logo.png");
-  background-position: var(--bX) var(--bY);
-  background-size: 40rem auto;
+  background-size: 10rem auto;
   box-shadow: 0 0 3rem 0.5rem hsla(0, 0%, 0%, 0.2);
   transition: transform 0.6s 1s;
 }
@@ -73,7 +101,7 @@ export default {
   content: "";
   width: 2rem;
   height: 2rem;
-  border: 1px solid #fff;
+  border: 2px solid red;
   position: absolute;
   z-index: 2;
   opacity: 0.3;
@@ -92,10 +120,6 @@ export default {
   left: 2rem;
   border-top-width: 0;
   border-right-width: 0;
-}
-
-.container--active {
-  transition: none;
 }
 
 .wrap:hover .container::before,
